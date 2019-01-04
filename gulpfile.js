79909bc6b3e7f3b45e10fed7fedee8ddcde6cbe3
zhangmin <948967661@qq.com>
2019-01-04 19:59:00 +0800

    初始页面已完成

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var server = require("gulp-webserver");
+
+gulp.task("devSass", function() {
+    return gulp.src("./src/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./src/css"))
+})
+
+gulp.task("devServer", function() {
+    return gulp.src("src")
+        .pipe(server({
+            port: 3300
+        }))
+})
+
+gulp.task("watch", function() {
+    return gulp.watch("./src/scss/*.scss", gulp.series("devSass"));
+});
+
+gulp.task("dev", gulp.series("devSass", "devServer", "watch"));
